a7b463e1ac33b4688435d45a12166a2598f4f689
Mariyan Donchev <pi4aaa@abv.bg>
2026-04-12 21:31:45 +0300

    Update GameDetails.jsx

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -4,85 +4,100 @@ import { useParams } from "react-router-dom";
 import commentsApi from "../../api/comments-api";
 
 export default function GameDetails() {
-    const [game, setGame] = useState({});
-    const [username, setUsername] = useState('');
-    const [comment, setComment] = useState('');
-    const { gameId } = useParams();
+  const [game, setGame] = useState({});
+  const [username, setUsername] = useState("");
+  const [comment, setComment] = useState("");
+  const { gameId } = useParams();
 
-    useEffect(() => {
-        (async () => {
-            const result = await gamesAPI.getOne(gameId);
-            setGame(result);
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      const result = await gamesAPI.getOne(gameId);
+      setGame(result);
+    })();
+  }, []);
 
-    const commentSubmitHandler = async (e) => {
-        e.preventDefault();
+  const commentSubmitHandler = async (e) => {
+    e.preventDefault();
 
-        await commentsApi.create(gameId, username, comment)
-    }
+    const newComment = await commentsApi.create(gameId, username, comment);
 
-    return (
-        <section id="game-details">
-            <h1>Game Details</h1>
-            <div className="info-section">
+    setGame((prevState) => ({
+      ...prevState,
+      comments: {
+        ...prevState.comments,
+        [newComment._id]: newComment,
+      },
+    }));
 
-                <div className="game-header">
-                    <img className="game-img" src={game.imageUrl} />
-                    <h1>{game.title}</h1>
-                    <span className="levels">MaxLevel: {game.maxlevel}</span>
-                    <p className="type">{game.category}</p>
-                </div>
+    setUsername("");
+    setComment("");
+  };
 
-                <p className="text">{game.sumary}</p>
+  return (
+    <section id="game-details">
+      <h1>Game Details</h1>
+      <div className="info-section">
+        <div className="game-header">
+          <img className="game-img" src={game.imageUrl} />
+          <h1>{game.title}</h1>
+          <span className="levels">MaxLevel: {game.maxlevel}</span>
+          <p className="type">{game.category}</p>
+        </div>
 
-                {/* <!-- Bonus ( for Guests and Users ) --> */}
-                <div className="details-comments">
-                    <h2>Comments:</h2>
-                    <ul>
-                        {/* <!-- list all comments for current game (If any) --> */}
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
-                    </ul>
-                    {/* <!-- Display paragraph: If there are no games in the database --> */}
-                    <p className="no-comment">No comments.</p>
-                </div>
+        <p className="text">{game.sumary}</p>
 
-                {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
-                <div className="buttons">
-                    <a href="#" className="button">Edit</a>
-                    <a href="#" className="button">Delete</a>
-                </div>
-            </div>
+        {/* <!-- Bonus ( for Guests and Users ) --> */}
+        <div className="details-comments">
+          <h2>Comments:</h2>
+          <ul>
+            {Object.keys(game.comments || {}).length > 0 ? (
+              Object.values(game.comments).map((comment) => (
+                <li key={comment._id} className="comment">
+                  <p>
+                    {comment.username}: {comment.text}
+                  </p>
+                </li>
+              ))
+            ) : (
+              <p className="no-comment">No comments.</p>
+            )}
+          </ul>
+        </div>
 
-            {/* <!-- Bonus --> */}
-            {/* <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
-            <article className="create-comment">
-                <label>Add new comment:</label>
-                <form className="form" onSubmit={commentSubmitHandler}>
-                    <input
-                        type="text"
-                        placeholder="Pesho"
-                        name="username"
-                        onChange={(e) => setUsername(e.target.value)}
-                        value={username}
-                    />
+        {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
+        <div className="buttons">
+          <a href="#" className="button">
+            Edit
+          </a>
+          <a href="#" className="button">
+            Delete
+          </a>
+        </div>
+      </div>
 
-                    <textarea
-                        name="comment"
-                        placeholder="Comment......"
-                        onChange={(e) => setComment(e.target.value)}
-                        value={comment}
-                    ></textarea>
+      {/* <!-- Bonus --> */}
+      {/* <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
+      <article className="create-comment">
+        <label>Add new comment:</label>
+        <form className="form" onSubmit={commentSubmitHandler}>
+          <input
+            type="text"
+            placeholder="Pesho"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
 
-                    <input className="btn submit" type="submit" value="Add Comment" />
-                </form>
-            </article>
+          <textarea
+            name="comment"
+            placeholder="Comment......"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          ></textarea>
 
-        </section>
-    )
+          <input className="btn submit" type="submit" value="Add Comment" />
+        </form>
+      </article>
+    </section>
+  );
 }
